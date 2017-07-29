a50d9f670d0eb183cc92d87423f8fb345be9f0b2
Alexander Grothe <seahawk1986@hotmail.com>
2017-07-29 10:11:08 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pydbus2vdr',
+      version='0.0.1',
+      description='A module to communicate with vdr-plugin-dbus2vdr using pydbus',
+      author='Alexander Grothe',
+      author_email='seahawk1986@gmx.de',
+      url='https://github.com/seahawk1986/python-pydbus2vdr',
+      py_modules=['pydbus2vdr'],
+      )
