59679ea5e2264d087a8f9e8b169eaf4ba224ae33
Alexander Grothe <seahawk1986@gmx.de>
2018-10-26 19:06:30 +0200

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pydbus2vdr',
-      version='0.0.1',
+      version='0.0.8',
       description='A module to communicate with vdr-plugin-dbus2vdr using pydbus',
       author='Alexander Grothe',
       author_email='seahawk1986@gmx.de',
